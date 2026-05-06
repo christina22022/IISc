8c242d99a41fbb2de99b317db4eb97ce5a57ed98
unknown <martinthomas8281@gmail.com>
2026-05-06 11:52:12 +0530

    ip cahnge

--- Dashboard/dashboard_project/app.py
+++ Dashboard/dashboard_project/app.py
@@ -694,7 +694,7 @@ def ask_ollama(user_question):
     prompt = build_prompt(user_question)
     try:
         response = requests.post(
-            "http://localhost:11434/api/generate",
+            "http://172.25.32.117:11434/api/generate",
             json={
                 "model": "gemma",
                 "prompt": prompt,
